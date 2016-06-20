4fdebb13e58839b2262023867e0fd3483007b728
NathanPeterson <nalexpeterson@gmail.com>
2016-06-20 15:37:46 -0400

    Add ng-submit to the form, add ng-model to each input, add mutant constuctor to form

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -7,6 +7,22 @@
 
     function MutantListController(){
       var vm = this;
+
+      vm.addMutant = addMutant;
+
       vm.mutants = ['deadpool', 'gambit', 'nightcrawler'];
+      vm.newMutant = new Mutant();
+
+      function Mutant(){
+        this.name = '';
+        this.phoneNumber = '';
+        this.topic = '';
+        this.notified = false;
+        this.complete = false;
+      }
+
+      function addMutant(){
+        vm.mutants.push(vm.newMutant.name);
+      }
     }
 })();
