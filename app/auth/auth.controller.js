7232de66dc79b19db65cd5ef06aa7fc423f8726b
NathanPeterson <nalexpeterson@gmail.com>
2016-06-22 13:47:38 -0400

    Create register function, Inject firebaseAuth into AuthController

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -5,10 +5,19 @@
     .module('mutantApp.auth')
     .controller('AuthController', AuthController);
 
-  AuthController.$inject =[];
+  AuthController.$inject =['$firebaseAuth'];
 
-  function AuthController(){
+  function AuthController($firebaseAuth){
     var vm =this;
-    
+    var auth = $firebaseAuth();
+    vm.register = register;
+    vm.user = {
+      email: '',
+      password: ''
+    }
+
+    function register(user){
+      return auth.$createUserWithEmailAndPassword(user.email, user.password);
+    }
   }
 })();
