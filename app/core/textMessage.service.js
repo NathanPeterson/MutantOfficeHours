21a897a56d4c906ca98c725ff49fa09a02a7a33d
NathanPeterson <nalexpeterson@gmail.com>
2016-06-23 14:05:32 -0400

    Create text message service and wire it up.

---
+++ app/core/textMessage.service.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.core')
+    .factory('textMessageService', textMessageService);
+
+  function textMessageService(){
+    var service = {};
+
+
+    return service;
+  }
+}());
