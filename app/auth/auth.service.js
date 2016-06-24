3bb17d646101b71654efd82ea04277db56eee4d3
NathanPeterson <nalexpeterson@gmail.com>
2016-06-24 15:15:36 -0400

    Add login and logout properties to authService. Refactor login and logout functions in AuthController. Remove unnecessary auth variable and dependency in AuthController

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -11,7 +11,8 @@
     var auth = $firebaseAuth();
     var service = {
       register: register,
-
+      login: login,
+      logout: logout,
     };
 
     return service;
@@ -19,6 +20,14 @@
     ////////////////////
     function register(user){
       return auth.$createUserWithEmailAndPassword(user.email, user.password);
+    }
+
+    function login(user){
+      return auth.$signInWithEmailAndPassword(user.email, user.password);
+    }
+
+    function logout(){
+      auth.$signOut();
     }
   }
 }());
